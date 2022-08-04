7b77cfd2c8cba90ba66ed07a164a6ab8c1fe997e
Ionut Vasile <email@iovasile.dev>
2022-08-04 23:01:35 +0300

    Initial Commit

---
+++ CleanArchitectureTodo/CleanArchitectureTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureTodoApp.swift
+//  CleanArchitectureTodo
+//
+//  Created by Ionut Vasile on 04.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
